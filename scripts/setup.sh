2eedd90a9f43e5baf1aab31ea191696cc9790201
rusty-vibess <99050442+rusty-vibess@users.noreply.github.com>
2025-10-04 15:46:27 +0100

    fix: Correct RMW impl defined

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,4 +7,6 @@ git clone -b humble https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
 git clone -b humble https://github.com/ROBOTIS-GIT/turtlebot3.git
 cd ~/turtlebot3_ws
 colcon build --symlink-install
+echo 'source /opt/ros/humble/setup.bash' >> ~/.bashrc 
+echo 'export RMW_IMPLEMENTATION=rmw_fastrtps_cpp' >> ~/.bashrc
 source ~/.bashrc
